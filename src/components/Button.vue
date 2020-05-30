3309de2952623f71a79bf7d1855d80ef719c40c9
pberrio <berriop@gmail.com>
2020-05-30 13:20:28 -0500

    llamado de api

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,15 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">        
-      <template v-slot:activator="{ on }">      
-        <v-btn color="#3A899A" dark v-on="on" style="margin: 20px 0">Formulario</v-btn>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn color="#3A899A" dark v-on="on" style="margin: 20px 0"
+          >Formulario</v-btn
+        >
       </template>
       <v-card>
         <v-toolbar dark color="#3A899A">
@@ -16,17 +23,23 @@
           </v-toolbar-items>
         </v-toolbar>
         <v-list three-line subheader>
-          <v-subheader>Formulario  paciente</v-subheader>
+          <v-subheader>Formulario paciente</v-subheader>
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>Content filtering</v-list-item-title>
-              <v-list-item-subtitle>Set the content filtering level to restrict apps that can be downloaded</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Set the content filtering level to restrict apps that can be
+                downloaded</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>Password</v-list-item-title>
-              <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Require password for purchase or use password to restrict
+                purchase</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -39,7 +52,10 @@
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>Notifications</v-list-item-title>
-              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Notify me about updates to apps or games that I
+                downloaded</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
           <v-list-item>
@@ -48,7 +64,10 @@
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>Sound</v-list-item-title>
-              <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Auto-update apps at any time. Data charges may
+                apply</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
           <v-list-item>
@@ -57,7 +76,9 @@
             </v-list-item-action>
             <v-list-item-content>
               <v-list-item-title>Auto-add widgets</v-list-item-title>
-              <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Automatically add home screen widgets</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -67,14 +88,33 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        dialog: false,
-        notifications: false,
-        sound: true,
-        widgets: false,
-      }
-    },
-  }
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      notifications: false,
+      sound: true,
+      widgets: false,
+      variable: null,
+      section: null
+    };
+  },
+  mounted() {
+    axios
+      .get('https://formularios-prueba-tecnica-6ihrk4y23q-ue.a.run.app/formularios/api/v3/prueba_tecnica')
+      .then(({ data }) => {
+        if (data && data.data && data.data.data) {
+          console.log(data.data.data);
+          const { variable, section } = data.data.data;
+          this.variable = variable;
+          this.section = section;
+        }
+        this.variable = null;
+        this.section = null;
+        return;
+      }).catch(error => console.log(error));
+  },
+};
 </script>
